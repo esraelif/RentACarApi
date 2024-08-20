80f76f46be0aeaa9147d67b6971868cef17d7ce7
esraelif <elifesratunca@gmail.com>
2024-08-20 12:26:22 +0200

    "Added Car controller and routes, updated Swagger definitions"

--- src/routes/car.js
+++ src/routes/car.js
@@ -11,7 +11,12 @@ const car = require('../controllers/car')
 //URL: /cars
 router.route('/')
     .get(car.list)
-    .post(permissions.isLogin)
+    .post(permissions.isStaff, car.create)
+router.route('/:id')
+    .get(car.read)
+    .put(permissions.isStaff, car.update)
+    .patch(permissions.isStaff, car.update)
+    .delete(permissions.isAdmin, car.delete)
 
 
 
