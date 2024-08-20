36e40ed64e6f2eb59cbea05ef46a786e8eec8493
esraelif <elifesratunca@gmail.com>
2024-08-20 14:55:30 +0200

    "Added routes and middleware for reservation management"

--- src/routes/reservation.js
+++ src/routes/reservation.js
@@ -1,3 +1,4 @@
+
 "use strict"
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
@@ -5,3 +6,22 @@
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 // routes/reservation:
+
+const reservation = require('../controllers/reservation')
+
+const permissions = require('../middlewares/permissions')
+
+// URL: /reservations
+
+router.route('/')
+    .get(permissions.isLogin, reservation.list)
+    .post(permissions.isLogin, reservation.create)
+
+router.route('/:id')
+    .get(permissions.isLogin, reservation.read)
+    .put(permissions.isStaff, reservation.update)
+    .patch(permissions.isStaff, reservation.update)
+    .delete(permissions.isAdmin, reservation.delete)
+
+/* ------------------------------------------------------- */
+module.exports = router
